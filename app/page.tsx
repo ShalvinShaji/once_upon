f8413e251fcec68454355e88c429d5b922bf4770
Shalvin <shalvinshaji283@gmail.com>
2024-11-23 19:44:12 +0530

    header completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Header from "@/components/Header";
+import StoryBuilder from "@/components/StoryBuilder";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p className="animate-pulse text-xl">
-        We are starting AI story builder...
-      </p>
+    <div className="dark flex flex-col items-center justify-center min-h-screen">
+      <div className="flex-1 flex items-center justify-center">
+        <Header />
+      </div>
+      <div className="flex-1 flex items-start justify-center">
+        <StoryBuilder />
+      </div>
     </div>
   );
 }
---
+++ components/StoryBuilder.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const StoryBuilder = () => {
+  return (
+    <div>
+      <h1>
+        "Oops! 🚧 Our Website is Still Under Construction{" "}
+        <span className="animate-pulse">!</span> "
+      </h1>
+    </div>
+  );
+};
+
+export default StoryBuilder;
